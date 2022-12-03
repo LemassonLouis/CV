3782a3a77b7b528177f150adaeee8549c266b603
LEMASSON Louis <galou2608@gmail.com>
2022-12-03 16:57:56 +0100

    Well...
    - Remove JsPDF library.
    - Adding html2pdf library.
    
    - Other changes to make it work (with server only)

--- js/download.js
+++ js/download.js
@@ -1,41 +1,32 @@
 async function download() {
-    console.log("download()");
-    var cv = document.getElementById("CV");
+
+    const downloadButton = document.getElementById("download");
+    const cv = document.getElementById("CV");
+    const title = document.getElementsByTagName("title")[0].innerText;
+
+    downloadButton.style.display = "none";
+    cv.classList.remove("sheet");
+    window.scrollTo(0,0);
     
-    const doc = new jsPDF();
-
-    await html2canvas(cv, {
-        allowTaint: true,
-        useCORS: false,
-        onrendered: function(canvas) {
-            console.log(canvas.toDataURL("image/png"));
-            doc.addImage(canvas.toDataURL("image/png"), 'PNG', 0, 0, 210, 297);
+    const opt = {
+        margin: [0, -2.3, 0, 0],
+        filename: `${title}.pdf`,
+        image: {
+            type: 'jpeg',
+            quality: 1
+        },
+        html2canvas: {
+            // scale: 6
+            // scale: 5
+            // scale: 4
+            scale: 3
+            // scale: 2
+            // scale: 1
         }
-    });
+    };
 
-    doc.save("Document.pdf");
+    await html2pdf().set(opt).from(cv).save();
 
-    // doc.html(cv, {
-    //     html2canvas: {
-    //         useCORS: true
-    //     },
-    //     callback: function (doc) {
-    //         doc.save();
-    //     },
-    // });
-
-    // doc.fromHTML(cv);
-    // doc.save("Document.pdf");
-
-    // var opt = {
-    //     margin:       1,
-    //     filename:     'myfile.pdf',
-    //     image:        { type: 'jpeg', quality: 1 },
-    //     html2canvas:  { scale: 2 },
-    //     jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-    // };
-
-    // // html2pdf().set(opt).from(cv).save();
-    // html2pdf().from(cv).save();
-
+    downloadButton.style.display = "";
+    cv.classList.add("sheet");
 }
